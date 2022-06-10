d03d8e3bb339d3b2bbe29330652a617a0d82d683
kucherol <kucherol2108@gmail.com>
2022-06-10 16:03:42 +0300

    more functionality added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import { Component } from 'react';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state ={
-      monsters: [
-        {
-          name: 'Linda'
-        },
-        {
-          name: 'Ray'
-        },
-        {
-          name: 'Jack'
-        },
-      ]
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState(() =>{
+        return {monsters: users}
+      },));
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+  this.setState(() => {
+    return { searchField };
+  })
+  };
+
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    });
+    
     return (
       <div className="App">
-        
+        <h1 className='app-title'>Monsters App</h1>
+        <SearchBox onChangeHandler={this.onSearchChange} placeholder={'Search monsters'} className={'monsters-search-box'} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
